6ef9b640d5233d606c6520737c723f11b84a95fe
Nikola Stanchev <nikigt3@gmail.com>
2025-07-31 18:04:50 +0300

    Initial Commit

---
+++ NanitBirthdayCard/NanitBirthdayCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanitBirthdayCardApp.swift
+//  NanitBirthdayCard
+//
+//  Created by Nikola Stanchev on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct NanitBirthdayCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NanitBirthdayCardTests/NanitBirthdayCardTests.swift
@@ -0,0 +1,17 @@
+//
+//  NanitBirthdayCardTests.swift
+//  NanitBirthdayCardTests
+//
+//  Created by Nikola Stanchev on 7/31/25.
+//
+
+import Testing
+@testable import NanitBirthdayCard
+
+struct NanitBirthdayCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
