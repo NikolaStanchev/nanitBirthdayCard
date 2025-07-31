0afbfc3d3f3799512c946747f094530f6a591f30
Nikola Stanchev <nikigt3@gmail.com>
2025-07-31 18:46:57 +0300

    feat: create light and dark mode icon

---
+++ NanitBirthdayCard/InputFormView.swift
@@ -0,0 +1,18 @@
+//
+//  InputFormView.swift
+//  NanitBirthdayCard
+//
+//  Created by Nikola Stanchev on 7/31/25.
+//
+
+import SwiftUI
+
+struct InputFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputFormView()
+}
